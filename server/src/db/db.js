b21cc9a4ef530531fe7749ba7ab6e78fb3617143
Nitin Kumar <nitinkumar.bacf@gmail.com>
2026-06-03 01:11:55 +0530

    Deployment ready

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -5,11 +5,21 @@ dotenv.config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT) || 3306,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD || "",
+  password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
+  queueLimit: 0,
+
+  ...(process.env.DB_SSL === 'true'
+    ? {
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
+    : {}),
 });
 
 export default pool;
